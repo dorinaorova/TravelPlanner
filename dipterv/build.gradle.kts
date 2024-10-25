ad7b1da2d25c584598e3b7bc8cf320253c52c764
dorinaorova <71429144+dorinaorova@users.noreply.github.com>
2024-10-25 11:24:07 +0200

    update plugin version

--- dipterv/build.gradle.kts
+++ dipterv/build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	id("org.springframework.boot") version "3.2.4"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.10"    // Change to 1.9.10
+	id("io.spring.dependency-management") version "1.0.13.RELEASE"
+	kotlin("jvm") version "1.9.10"
 	kotlin("plugin.spring") version "1.9.10"
 	kotlin("plugin.jpa") version "1.9.10"
 }
